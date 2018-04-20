ee2c7710e5d4d886a1a3da96f6fcceeeeeb12d81
Spider <Spiderixius@users.noreply.github.com>
2018-04-20 12:45:41 +0200

    Updated proximity to send data to sigfox.

--- Proximity/Proximity.ino
+++ Proximity/Proximity.ino
@@ -1,51 +1,153 @@
-/*
-    SmeProximity Library - Proximity.ino
-
-    Demonstrate how to detect the proximity of an object in polling mode.
-
-    And the ends the green led continuously blink.
-
-    created 27 Apr 2015
-    by Mik (smkk@axelelettronica.it)
-
-    This example is in the public domain
-    https://github.com/ameltech
-
-    VL6180  more information available here:
-    http://www.st.com/web/catalog/mmc/FM132/SC626/PF260441?icmp=pf260441_pron_p3609p_sep2014&sc=proximitysensor
-*/
-
-#include <Wire.h>
+#include <TimeLib.h>
 
 #include <VL6180.h>
+#include <Wire.h>
 #include <Arduino.h>
+#include <SmeNfc.h>
+#include <SmeSFX.h>
+#include <Time.h>
 
+#define SIGFOX_FRAME_LENGTH 12
 
-// the setup function runs once when you press reset or power the board
+bool DEBUG = true;
+
+time_t t = now();
+
+bool isStart = true;
+
+struct data {
+  int startTime;
+  int endTime;
+};
+
+data frame;
+
 void setup() {
-  Wire.begin();
+  ledGreenLight(HIGH);
+  frame.startTime = 0;
+  frame.endTime = 0;
+  
+  if(DEBUG) {
+      SerialUSB.begin(115200);
+      SerialUSB.println("Welcome");
+        while (!SerialUSB) {
+        ; 
+      }
+  }   
+  smeProximity.begin();
+  
+  SigFox.begin(19200);
+  initSigfox();
+  
+  ledGreenLight(LOW);
+}
 
-  if (!smeProximity.begin()) {
-    while (1) {
-      ; // endless loop due to error on VL6180 initialization
+void loop() {  
+  checkSomethingOn();
+  
+  if(DEBUG) {
+    SerialUSB.println("Thats one loop!");
+  }
+  delay(100);
+  ledGreenLight(LOW);
+  ledRedLight(LOW);
+}
+
+void checkSomethingOn() {
+  int proximity = smeProximity.rangePollingRead();
+  SerialUSB.print("Proximity ");
+  SerialUSB.println(proximity);
+  
+  int threshold = 50;
+
+  if(proximity < threshold && isStart){
+    isStart = false;
+    frame.startTime = second(now());
+    SerialUSB.print("Second t1 ");
+    SerialUSB.println(second(now()));
+    SerialUSB.print("startTime ");
+    SerialUSB.println(frame.startTime);
+  } else if (proximity > threshold && !isStart){
+    frame.endTime = second(now());
+    SerialUSB.print("Second t2 ");
+    SerialUSB.println(second(now()));
+    isStart = true;
+    SerialUSB.print("endTime ");
+    SerialUSB.println(frame.endTime);
+    sendSigfox(&frame, sizeof(data));
+  } 
+}
+
+void initSigfox(){
+  SigFox.print("+++");
+  while (!SigFox.available()){
+    delay(100);
+  }
+  while (SigFox.available()){
+    byte serialByte = SigFox.read();
+    if (DEBUG){
+      SerialUSB.print("Serial Byte ");
+      SerialUSB.println(serialByte);
+    }
+  }
+  if (DEBUG){
+    SerialUSB.println("\n ** Setup OK **");
+  }
+}
+
+bool sendSigfox(const void* data, uint8_t len){
+  String frame = getSigfoxFrame(data, len);
+  String status = "";
+  char output;
+  if (DEBUG){
+    SerialUSB.print("AT$SF=");
+    SerialUSB.println(frame);
+  }
+  SigFox.print("AT$SF=");
+  SigFox.print(frame);
+  SigFox.print("\r");
+  while (!SigFox.available());
+  
+  while(SigFox.available()){
+    output = (char)SigFox.read();
+    status += output;
+    delay(10);
+  }
+  if (DEBUG){
+    SerialUSB.print("Status \t");
+    SerialUSB.println(status);
+  }
+  if (status == "OK\r"){
+    //Success :)
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+String getSigfoxFrame(const void* data, uint8_t len){
+  String frame = "";
+  SerialUSB.print("Frame ");
+  SerialUSB.println(frame);
+  uint8_t* bytes = (uint8_t*)data;
+  //SerialUSB.println(bytes);
+  
+  if (len < SIGFOX_FRAME_LENGTH){
+    //fill with zeros
+    uint8_t i = SIGFOX_FRAME_LENGTH;
+    while (i-- > len){
+      frame += "00";
     }
   }
 
-  SerialUSB.begin(115200);
-
-}
-
-// the loop function runs over and over again forever
-void loop() {
-  char ligth = smeProximity.rangePollingRead();
-
-  if (ligth == 255) {
-    SerialUSB.println("Infinity");
-  } else {
-    SerialUSB.print(ligth, DEC);
-    SerialUSB.println(" mm");
+  //0-1 == 255 --> (0-1) > len
+  for(uint8_t i = len-1; i < len; --i) {
+    if (bytes[i] < 16) {frame+="0";}
+    frame += String(bytes[i], HEX);
+    SerialUSB.print("Frame ");
+    SerialUSB.println(frame);
   }
-
-  delay(2000);              // wait for 2 seconds
-
+  
+  return frame;
 }
